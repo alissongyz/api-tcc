2406172aeed6209543e0bce24568ae6c21b1d8db
alisson <alisson.arruda@techsavvy.com.br>
2022-07-14 11:08:21 -0300

    remove pagination material

--- src/controllers/material-controller.ts
+++ src/controllers/material-controller.ts
@@ -22,13 +22,6 @@ class MaterialController {
       builder.orderBy('material.name', sort.toLowerCase())
     }
 
-    // APLICANDO REGRA DE PAGINAÇÃO NO GET
-    const page: number = parseInt(req.query.page as any) || 1
-    const pageSize = 8
-    const total = await builder.getCount()
-
-    builder.offset((page - 1) * pageSize).limit(pageSize)
-
     return res.send(
       await builder.getMany(), // RETORNA TODOS OS ITEMS DO BANCO
     )
